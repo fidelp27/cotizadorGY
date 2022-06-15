335cb2556ca2dfd05d782b675a2ab3849d9cfc70
Fide <fidel2702@gmail.com>
2022-06-15 11:26:25 -0300

    commercial error

--- src/App.js
+++ src/App.js
@@ -80,7 +80,6 @@ const App = () => {
               setDatos={setDatos}
               setLoading={setLoading}
               setTiempo={setTiempo}
-              tiempo={tiempo}
             />
           }
         />
